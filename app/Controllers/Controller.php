c2e2176c07ac454a39bc7626fa4da858a7086f5d
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2026-01-07 17:08:54 -0300

    feat: Create Controller super class, update api result format

---
+++ app/Controllers/Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Database\DatabaseConnector;
+
+class Controller {
+    protected $dbConn;
+
+    public function __construct()
+    {
+        $dbConn = new DatabaseConnector;
+        $this->dbConn = $dbConn->createDbConnection();
+    }
+
+    protected function deliverResponse($responseCode, $data = null)
+    {
+        header('Content-Type: application/json; charset=utf-8');
+        http_response_code($responseCode);
+
+        exit(json_encode($data, 0));
+    }
+}
--- app/Controllers/CustomerController.php
+++ app/Controllers/CustomerController.php
@@ -2,23 +2,13 @@
 
 namespace App\Controllers;
 
-use App\Database\DatabaseConnector;
-
-class CustomerController
+class CustomerController extends Controller
 {
-    protected $dbConn;
-
-    public function __construct()
-    {
-        $dbConn = new DatabaseConnector;
-        $this->dbConn = $dbConn->createDbConnection();
-    }
-
     public function index()
     {
         $query = $this->dbConn->query('SELECT * FROM customers');
         $data = $query->fetchAll();
 
-        echo json_encode($data, 0);
+        $this->deliverResponse(200, $data);
     }
 }
