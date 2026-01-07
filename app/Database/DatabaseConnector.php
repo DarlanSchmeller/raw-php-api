a79c652d81c4ed586be43e9d501d11dbc8277752
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2026-01-07 11:48:07 -0300

    feat: Load env variables and add DatabaseConnector for app bootstrap

---
+++ app/Database/DatabaseConnector.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database;
+
+use PDO;
+use PDOException;
+use Exception;
+
+class DatabaseConnector
+{
+    private $dbHost;
+    private $dbPort;
+    private $dbName;
+    private $dbUser;
+    private $dbPassword;
+
+    public function __construct()
+    {
+        $this->dbHost = $_ENV['DB_HOST'];
+        $this->dbPort = $_ENV['DB_PORT'];
+        $this->dbName = $_ENV['DB_NAME'];
+        $this->dbUser = $_ENV['DB_USER'];
+        $this->dbPassword = $_ENV['DB_PASSWORD'];
+    }
+
+    public function createDbConnection(): PDO
+    {
+        try {
+            $dsn = "mysql:host=$this->dbHost;dbname=$this->dbName;charset=utf8mb4";
+            $dbConn = new PDO(
+                $dsn,
+                $this->dbUser,
+                $this->dbPassword,
+            );
+    
+            return $dbConn;
+        } catch (PDOException $e) {
+            throw new Exception('Error connecting to the database: ' . $e->getMessage());
+        }
+    }
+}
