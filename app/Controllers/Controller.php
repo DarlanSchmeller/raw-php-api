77c2c7251922737153a7c21b8db5c7ad459f0f47
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2026-01-09 15:52:03 -0300

    refactor: Remove database connection setup from base controller

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -5,14 +5,6 @@ namespace App\Controllers;
 use Database\DatabaseConnector;
 
 class Controller {
-    protected $dbConn;
-
-    public function __construct()
-    {
-        $dbConn = new DatabaseConnector;
-        $this->dbConn = $dbConn->createDbConnection();
-    }
-
     protected function deliverResponse($responseCode, $data = null): void
     {
         http_response_code($responseCode);
